86cf96a5dec49f7f98670e82cdfab35938fb0ffc
kogkoba <kc.koba@gmail.com>
2025-09-09 16:03:19 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 window.APP_CONFIG = {
   SPREADSHEET_ID: "1L3dUsXqIPQSAhZE1VbduKXJeABrx2Ob3w1YfqXG4aA",
   SUBJECT_GIDS: {
-    "国語": 0,          // ←実際のgidに差し替え
+    "国語": 162988483,          // ←実際のgidに差し替え
     "算数": 0,          // 同上
-    "理科": 0,          // 同上
+    "理科": 1839969673,          // 同上
     "社会": 2143649641  // 例
   },
   GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbx9PKDGh-a5AkeSz5sPlJlCsJZSZGYa7iqCnIcLaCFAk1iHo0mi7T-RlLbZcTzWf9HBJw/exec",
